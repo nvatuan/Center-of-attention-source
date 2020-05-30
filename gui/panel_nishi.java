c5d6509f907c287637e78fc993ffba249ffd93f7
qhuy4919 <65387927+qhuy4919@users.noreply.github.com>
2020-05-31 00:45:12 +0700

    Chia các component thành tệp riêng

---
+++ gui/panel_nishi.java
@@ -0,0 +1,8 @@
+package Controller.panel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public interface panel_nishi {
+     JPanel panel_nishi();
+}
---
+++ gui/thong_so.java
@@ -0,0 +1,6 @@
+package Controller;
+
+public interface thong_so {
+    public static int W = 800;
+    public static int H = 700;
+}
