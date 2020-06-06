3282fdb4ca0b72917e0f7ca2e252123362deaf27
nvat <24392632+nvatuan@users.noreply.github.com>
2020-06-06 17:26:55 +0700

    Replaced function with lamda

--- algo/source/Image_implem1.cpp
+++ algo/source/Image_implem1.cpp
@@ -1,8 +1,6 @@
 #include "../header/ImageDataStructure.hpp"
 
-static std::vector<unsigned> _DEPTH = std::vector<unsigned>();
-static unsigned bfs_max_depth_of (unsigned row, unsigned col);
-
+/*
 static unsigned bfs_max_depth_of (unsigned row, unsigned col, const Image& Img) { 
     std::queue< std::pair<std::pair<int, int>, unsigned> > Q;
     std::vector<bool> Visited = std::vector<bool> (Img.width * Img.height, false);
@@ -32,10 +30,42 @@ static unsigned bfs_max_depth_of (unsigned row, unsigned col, const Image& Img) 
 
     return -1;
 }
+*/
 
 std::vector<unsigned> Image::central_pixels (unsigned colour) const {
     unsigned current_max_depth = 0;
-    _DEPTH = std::vector<unsigned> (width * height, 0);
+    std::vector<unsigned> _DEPTH = std::vector<unsigned> (width * height, 0);
+
+    auto bfs_max_depth_of = [&](unsigned row, unsigned col, const Image& Img) -> unsigned {
+        std::queue< std::pair<std::pair<int, int>, unsigned> > Q;
+        std::vector<bool> Visited = std::vector<bool> (Img.width * Img.height, false);
+        unsigned current_color = Img.pixels[Img.pair_to_imgidx(row, col)];
+
+        Q.push( std::make_pair(std::make_pair(row, col), 0) );
+        while (not Q.empty()) {
+            std::pair<int, int> coor = Q.front().first;
+            unsigned manhattan_dist = Q.front().second;
+            unsigned image_idx = Img.pair_to_imgidx(coor);
+            Q.pop();
+
+            if (    coor.first < 0 or coor.first == Img.height or
+                    coor.second < 0 or coor.second == Img.width or
+                    Img.pixels[image_idx] != current_color ) {
+                return manhattan_dist;
+            }
+
+            if ( Visited[image_idx] == true ) continue;
+            Visited[image_idx] = true;
+
+            Q.push( std::make_pair(std::make_pair(coor.first - 1, coor.second), manhattan_dist+1) );
+            Q.push( std::make_pair(std::make_pair(coor.first + 1, coor.second), manhattan_dist+1) );
+            Q.push( std::make_pair(std::make_pair(coor.first, coor.second - 1), manhattan_dist+1) );
+            Q.push( std::make_pair(std::make_pair(coor.first, coor.second + 1), manhattan_dist+1) );
+        }
+
+        return -1;
+    };
+
     for (int x = 0; x < height; x++) 
         for (int y = 0; y < width; y++) 
             if (pixels[pair_to_imgidx(x, y)] == colour) {
