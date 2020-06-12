19ed8640045cd73be9df6f5f6971aa740bad96a5
nvat <24392632+nvatuan@users.noreply.github.com>
2020-06-12 23:13:29 +0700

    Testing `src/main/ImageCentralPixels` | + results are in `log/` | + Haven't run the large tests though, got to fork those from the C++ branch |_______________________________________________________________________

---
+++ algo/build_script/build.sh
@@ -0,0 +1,12 @@
+##
+javac -d bin src/main/PairRowColumn/PairRowColumn.java
+javac -d bin -cp bin src/main/Image/Image.java
+javac -d bin -cp bin src/main/ImageCentralPixels/ImageCentralPixels.java
+
+javac -d bin -cp bin src/test/TestData/TestData.java
+javac -d bin -cp bin src/test/Tester/Tester.java
+
+java -cp bin test/Tester manual0 > log/manual0.txt
+java -cp bin test/Tester random0 > log/random0.txt
+java -cp bin test/Tester random1 > log/random1.txt
+java -cp bin test/Tester edge0 > log/edge0.txt
--- algo/src/test/Tester/Tester.java
+++ algo/src/test/Tester/Tester.java
@@ -1,11 +1,87 @@
 package test;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import main.ImageCentralPixels;
+import test.TestData;
 
 public class Tester {
+    public static ArrayList<TestData> tds = new ArrayList<TestData>();
+    public static void parse(String path) {
+        for (int counter = 0; ; counter++) {
+            File fi = new File(path + "in" + counter + ".txt");
+            File fo = new File(path + "out" + counter + ".txt");
+
+            // System.out.println("FILEIN> " + fi.getAbsolutePath());
+            // System.out.println("FILEOUT> " + fo.getAbsolutePath());
+
+            if (fi.exists() && fo.exists()) {
+                try {
+                    TestData td = new TestData(fi, fo);
+                    tds.add(td);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // System.out.println("Files do not exist. Stopping..");
+                break;
+            } 
+        }
+    }
+
+    public static boolean checker(int[] p, Integer[] j) {
+        Arrays.sort(p);
+        if (p.length != j.length) return false;
+        for (int i = 0; i < p.length; i++)
+            if (p[i] != ((int)j[i])) return false;
+        return true;
+    }
+
+    public static void runTest() {
+        System.out.println("There are " + tds.size() + " tests.");
+        int test = 0;
+
+        for (TestData td : tds) {
+            System.out.println("@Test" + test++);
+
+            int correct = 0;
+            int subtests = td.subtestInput.size();
+            long start = System.currentTimeMillis();
+
+            for (int subtest = 0; subtest < subtests; subtest++) {
+                int[] participant = td.img.centralPixels(td.subtestInput.get(subtest));
+                Integer[] jury = new Integer[0];
+                
+                jury = td.subtestJuryans.get(subtest).toArray(new Integer[0]);
+
+                if (checker(participant, jury)) {
+                    // System.out.println("Correct!");
+                    correct++;
+                } else {
+                    System.out.print("@Subtest" + subtest + " gives Wrong result!");
+                }
+            }
+
+            double testDuration = (System.currentTimeMillis() - start)*0.001;
+            System.out.println("Correct: " + correct + " subtest(s) out of " + subtests);
+            System.out.println("Test Execution time: " + String.format("%.4f", testDuration));
+            System.out.println("Test Average Execution time: " + String.format("%.4f", testDuration/subtests));
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length > 0) {
+            String fold = String.join(" ", args);
+            String path = "./exported/" + fold + "/";
+            
+            Tester.parse(path);
+            /*for (TestData td : tds) {
+                System.out.println(td);
+            }*/
 
+            Tester.runTest();
         } else {
             System.out.println("Please provide an argument.");
         }
