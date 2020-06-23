35fe8c94f6312b1bb37e7cf4908a30f9f2dac690
nvat <24392632+nvatuan@users.noreply.github.com>
2020-06-24 00:38:15 +0700

    Thuật toán đã hoàn thiện và đúng, merge để test sản phẩm (#3)
    * Created a new branch to store tests
    |_______________________________________________________________________
    
    * Populated `dev-algo-java`, ((code is not working though))
    | + Created basic directory structure, changes are not final.
    | + Forked `algo/test/export/` from `dev-algo-cpp`
    |_______________________________________________________________________
    
    * Implemented 3rd algo in Java, haven't been tested fully;
    | + `src/main/ImageCentralPixels` contains algorithm
    | + Others supplement class is defined in `src/main/*`
    | + Added build_script/
    | Tested some cases with hand produced correct results, now i'm writing a parser to check via files.
    |_______________________________________________________________________
    
    * Testing `src/main/ImageCentralPixels`
    | + results are in `log/`
    | + Haven't run the large tests though, got to fork those from the C++ branch
    |_______________________________________________________________________
    
    * Tested with large test.
    | It seemed edge1 second test is a bit too large..
    |_______________________________________________________________________

---
+++ algo/src/main/image/Image.java
@@ -0,0 +1,61 @@
+package main;
+
+import main.PairRowColumn;
+import java.util.Arrays;
+
+public class Image {
+    int[] pixels;
+    int width, height;
+    
+    // -- getters
+    public int getWidth() { return this.width; }
+    public int getHeight() { return this.height; }
+    public int[] getPixels() { return this.pixels; }
+
+    // -- constructor
+    public Image() {
+        width = height = 0;
+        pixels = null;
+    }
+
+    public Image(int w, int h) {
+        width = w; height = h;
+        pixels = new int[w*h];
+        Arrays.fill(pixels, 0);
+    }
+
+    public Image(int w, int h, int[] p) {
+        this.width = w;
+        this.height = h;
+        this.pixels = Arrays.copyOf(p, p.length);
+    }
+
+    public Image(Image img) {
+        this.width = img.getWidth();
+        this.height = img.getHeight();
+
+        int[] ref = img.getPixels();
+        this.pixels = Arrays.copyOf(ref, ref.length);
+    }
+
+    // -- functions
+    public int pairToImageIndex(int ih, int iw) { return ih * this.width + iw; }
+    public int pairToImageIndex(PairRowColumn p) { return p.getRow() * this.width + p.getCol(); }
+    public PairRowColumn imageIndexToPair(int idx) {
+        return new PairRowColumn(idx / this.width, idx % this.width);
+    }
+
+    // -- toString()
+    @Override
+    public String toString() {
+        String rep = "Width = " + this.width + " | Height = " + this.height + "\n";
+        rep += "Image = {\n";
+        for (int i = 0; i < this.width*this.height; i++) {
+            rep += this.pixels[i];
+            if ((i + 1) % width == 0) rep += "\n";
+            else rep += " ";
+        }
+        rep += "}\n";
+        return rep;
+    }
+}
